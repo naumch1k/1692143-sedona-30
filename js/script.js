f6abe59e9a51c004f2349ddd5916b296186d3439
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-03-16 15:26:27 -0500

    lang eng

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const interestedButton = document.querySelector('.interested__button');
 const modal = document.querySelector('.modal');
 const searchForm = modal.querySelector('.search-form');
-const moveIn = modal.querySelector('[name=move-in]');
-const moveOut = modal.querySelector('[name=move-out]');
+const checkIn = modal.querySelector('[name=check-in]');
+const checkOut = modal.querySelector('[name=check-out]');
 const adults = modal.querySelector('[name=adults]');
-const children = modal.querySelector('[name=children]');
+const kids = modal.querySelector('[name=kids]');
 const adultsStorage = localStorage.getItem("adults");
-const childrenStorage = localStorage.getItem("children");
+const kidsStorage = localStorage.getItem("kids");
 const modalHidden = localStorage.getItem("modal-hidden");
 
 let isStorageSupport = true;
@@ -30,12 +30,12 @@ interestedButton.addEventListener("click", function (evt) {
     modal.classList.remove('modal_hidden');
     modal.classList.remove('modal_error');
     localStorage.setItem("modal-hidden", "false")
-    moveIn.focus({preventScroll:true});
+    checkIn.focus({preventScroll:true});
     if (adultsStorage) {
       adults.value = adultsStorage;
     }
-    if (childrenStorage) {
-      children.value = childrenStorage;
+    if (kidsStorage) {
+      kids.value = kidsStorage;
     }
   } else {
     modal.classList.add('modal_hidden');
@@ -44,7 +44,7 @@ interestedButton.addEventListener("click", function (evt) {
 });
 
 searchForm.addEventListener("submit", function (evt) {
-  if (!moveIn.value || !moveOut.value || !adults.value || !children.value) {
+  if (!checkIn.value || !checkOut.value || !adults.value || !kids.value) {
     evt.preventDefault();
     modal.classList.remove("modal_error");
     modal.offsetWidth = modal.offsetWidth;
@@ -52,7 +52,7 @@ searchForm.addEventListener("submit", function (evt) {
   } else {
     if (isStorageSupport) {
       localStorage.setItem("adults", adults.value);
-      localStorage.setItem("children", children.value);
+      localStorage.setItem("kids", kids.value);
     } 
   }
 });
